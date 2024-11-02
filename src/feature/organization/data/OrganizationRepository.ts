e56d9580cb62416297e3be25bfaf6b8d83e3a5ed
alireza <alireza@nativeblocks.io>
2024-11-02 21:54:32 +0300

    delete all project's files

--- src/feature/organization/data/OrganizationRepository.ts
+++
@@ -1,8 +0,0 @@
-import {ResultModel} from "../../../infrastructure/result/model/ResultModel";
-import {OrganizationModel} from "./OrganizationRepositoryImpl";
-
-export interface OrganizationRepository {
-  organizations(): Promise<ResultModel<OrganizationModel[]>>;
-  set(id: string): Promise<ResultModel<string>>;
-  get(): Promise<ResultModel<string>>;
-}
--- src/infrastructure/utility/FileUitl.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-export function createDefaultDir(directory: string, withHiddenForlder: boolean = true) {
-  const path = withHiddenForlder ? `${directory}/.nativeblocks` : `${directory}`;
-  if (!fs.existsSync(`${path}`)) {
-    fs.mkdirSync(`${path}`, { recursive: true });
-  }
-  return `${path}`;
-}
