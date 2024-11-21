7103cac14ad1561ff0881b8ae8659aabe20dc0b4
alireza <alireza@nativeblocks.io>
2024-11-22 01:16:41 +0300

    support code-gen for ts

--- cmd/codeGenModule/codeGen.go
+++ cmd/codeGenModule/codeGen.go
@@ -16,6 +16,7 @@ func CodeGenCmd() *cobra.Command {
 	}
 
 	cmd.AddCommand(genJSCmd())
+	cmd.AddCommand(genTSCmd())
 
 	return cmd
 }
@@ -63,8 +64,8 @@ func genJSCmd() *cobra.Command {
 					continue
 				}
 				name := key + "-Block"
-				block := generateJSClass(strcase.ToLowerCamel(name), component, "BLOCK")
-				if err := fm.SaveByteToFile(strcase.ToLowerCamel(name)+".js", []byte(block)); err != nil {
+				block := generateJSClass(strcase.ToCamel(name), component, "BLOCK")
+				if err := fm.SaveByteToFile(strcase.ToCamel(name)+".js", []byte(block)); err != nil {
 					return err
 				}
 			}
@@ -85,12 +86,95 @@ func genJSCmd() *cobra.Command {
 					continue
 				}
 				name := key + "-Action"
-				action := generateJSClass(strcase.ToLowerCamel(name), component, "ACTION")
-				if err := fm.SaveByteToFile(strcase.ToLowerCamel(name)+".js", []byte(action)); err != nil {
+				action := generateJSClass(strcase.ToCamel(name), component, "ACTION")
+				if err := fm.SaveByteToFile(strcase.ToCamel(name)+".js", []byte(action)); err != nil {
 					return err
 				}
 			}
 			fmt.Printf("JS classes generated: %v \n", baseDir)
+			return nil
+		},
+	}
+	cmd.Flags().StringVarP(&path, "path", "p", "", "Output path")
+	cmd.Flags().StringVarP(&blocksSchema, "blocksSchemaUrl", "b", "", "Blocks schema url")
+	cmd.Flags().StringVarP(&actionsSchema, "actionsSchemaUrl", "a", "", "Blocks schema url")
+	_ = cmd.MarkFlagRequired("path")
+	_ = cmd.MarkFlagRequired("blocksSchemaUrl")
+	_ = cmd.MarkFlagRequired("actionsSchemaUrl")
+	return cmd
+}
+
+func genTSCmd() *cobra.Command {
+	var path string
+	var blocksSchema string
+	var actionsSchema string
+	cmd := &cobra.Command{
+		Use:   "ts",
+		Short: "Generate TS",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			baseDir := fileutil.GetFileDir(path + "/")
+
+			fm, err := fileutil.NewFileManager(&baseDir)
+			if err != nil {
+				return err
+			}
+
+			blocks := make(map[string]interface{})
+			err = jsonutil.FetchJSONFromURL(blocksSchema, &blocks)
+			if err != nil {
+				return err
+			}
+			actions := make(map[string]interface{})
+			err = jsonutil.FetchJSONFromURL(actionsSchema, &actions)
+			if err != nil {
+				return err
+			}
+
+			for key, value := range blocks {
+				if key == "schema-version" {
+					continue
+				}
+				componentBytes, err := json.Marshal(value)
+				if err != nil {
+					fmt.Println("Error marshaling component:", err)
+					continue
+				}
+
+				var component Integration
+				err = json.Unmarshal(componentBytes, &component)
+				if err != nil {
+					fmt.Println("Error unmarshalling component:", err)
+					continue
+				}
+				name := key + "-Block"
+				block := generateTSClass(strcase.ToCamel(name), component, "BLOCK")
+				if err := fm.SaveByteToFile(strcase.ToCamel(name)+".ts", []byte(block)); err != nil {
+					return err
+				}
+			}
+			for key, value := range actions {
+				if key == "schema-version" {
+					continue
+				}
+				componentBytes, err := json.Marshal(value)
+				if err != nil {
+					fmt.Println("Error marshaling component:", err)
+					continue
+				}
+
+				var component Integration
+				err = json.Unmarshal(componentBytes, &component)
+				if err != nil {
+					fmt.Println("Error unmarshalling component:", err)
+					continue
+				}
+				name := key + "-Action"
+				action := generateTSClass(strcase.ToCamel(name), component, "ACTION")
+				if err := fm.SaveByteToFile(strcase.ToCamel(name)+".ts", []byte(action)); err != nil {
+					return err
+				}
+			}
+			fmt.Printf("TS classes generated: %v \n", baseDir)
 			return nil
 		},
 	}
