24fd9f4f51b650bc29eb37a4b3d83b763ddc3fcc
alireza <alireza@nativeblocks.io>
2024-11-22 16:17:44 +0300

    support php codeGen

--- cmd/codeGenModule/codeGen.go
+++ cmd/codeGenModule/codeGen.go
@@ -16,6 +16,7 @@ func CodeGenCmd() *cobra.Command {
 	}
 
 	cmd.AddCommand(genTSCmd())
+	cmd.AddCommand(genPHPCmd())
 
 	return cmd
 }
@@ -91,6 +92,89 @@ func genTSCmd() *cobra.Command {
 				}
 			}
 			fmt.Printf("TS classes generated: %v \n", baseDir)
+			return nil
+		},
+	}
+	cmd.Flags().StringVarP(&path, "path", "p", "", "Output path")
+	cmd.Flags().StringVarP(&blocksSchema, "blocksSchemaUrl", "b", "", "Blocks schema url")
+	cmd.Flags().StringVarP(&actionsSchema, "actionsSchemaUrl", "a", "", "Blocks schema url")
+	_ = cmd.MarkFlagRequired("path")
+	_ = cmd.MarkFlagRequired("blocksSchemaUrl")
+	_ = cmd.MarkFlagRequired("actionsSchemaUrl")
+	return cmd
+}
+
+func genPHPCmd() *cobra.Command {
+	var path string
+	var blocksSchema string
+	var actionsSchema string
+	cmd := &cobra.Command{
+		Use:   "php",
+		Short: "Generate PHP",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			baseDir := fileutil.GetFileDir(path + "/")
+
+			fm, err := fileutil.NewFileManager(&baseDir)
+			if err != nil {
+				return err
+			}
+
+			blocks := make(map[string]interface{})
+			err = jsonutil.FetchJSONFromURL(blocksSchema, &blocks)
+			if err != nil {
+				return err
+			}
+			actions := make(map[string]interface{})
+			err = jsonutil.FetchJSONFromURL(actionsSchema, &actions)
+			if err != nil {
+				return err
+			}
+
+			for key, value := range blocks {
+				if key == "schema-version" {
+					continue
+				}
+				componentBytes, err := json.Marshal(value)
+				if err != nil {
+					fmt.Println("Error marshaling component:", err)
+					continue
+				}
+
+				var component Integration
+				err = json.Unmarshal(componentBytes, &component)
+				if err != nil {
+					fmt.Println("Error unmarshalling component:", err)
+					continue
+				}
+				name := key + "-Block"
+				block := generatePHPClass(strcase.ToCamel(name), component, "BLOCK")
+				if err := fm.SaveByteToFile(strcase.ToCamel(name)+".php", []byte(block)); err != nil {
+					return err
+				}
+			}
+			for key, value := range actions {
+				if key == "schema-version" {
+					continue
+				}
+				componentBytes, err := json.Marshal(value)
+				if err != nil {
+					fmt.Println("Error marshaling component:", err)
+					continue
+				}
+
+				var component Integration
+				err = json.Unmarshal(componentBytes, &component)
+				if err != nil {
+					fmt.Println("Error unmarshalling component:", err)
+					continue
+				}
+				name := key + "-Action"
+				action := generatePHPClass(strcase.ToCamel(name), component, "ACTION")
+				if err := fm.SaveByteToFile(strcase.ToCamel(name)+".php", []byte(action)); err != nil {
+					return err
+				}
+			}
+			fmt.Printf("PHP classes generated: %v \n", baseDir)
 			return nil
 		},
 	}
