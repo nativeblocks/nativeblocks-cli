3a18474cdc37b80e6d2b1c5effbb8e15aa6249fd
alireza <alireza@nativeblocks.io>
2024-11-24 01:42:44 +0300

    support auth with accessToken

--- cmd/authModule/authRepository.go
+++ cmd/authModule/authRepository.go
@@ -1,7 +1,11 @@
 package authModule
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"errors"
+	"log"
+	"strings"
 
 	"github.com/nativeblocks/cli/library/fileutil"
 	"github.com/nativeblocks/cli/library/graphqlutil"
@@ -49,6 +53,39 @@ func Authenticate(fm fileutil.FileManager, regionUrl, username, password string)
 	authConfig := AuthModel{
 		AccessToken: authResponse.AuthLogin.AccessToken,
 		Email:       authResponse.AuthLogin.Email,
+	}
+
+	if err := fm.SaveToFile(authCacheFileName, authConfig); err != nil {
+		return nil, errors.New("failed to save auth config: " + err.Error())
+	}
+
+	return &authConfig, nil
+}
+
+func AuthenticateWithToken(fm fileutil.FileManager, accessToken string) (*AuthModel, error) {
+	parts := strings.Split(accessToken, ".")
+	if len(parts) != 3 {
+		log.Fatal("Invalid JWT token")
+	}
+	payload := parts[1]
+	padding := len(payload) % 4
+	if padding != 0 {
+		padding = 4 - padding
+	}
+	payload = payload + strings.Repeat("=", padding)
+	decodedPayload, err := base64.URLEncoding.DecodeString(payload)
+	if err != nil {
+		log.Fatal("Error decoding payload: ", err)
+	}
+	var claims map[string]interface{}
+	err = json.Unmarshal(decodedPayload, &claims)
+	if err != nil {
+		log.Fatal("Error unmarshaling payload: ", err)
+	}
+	eml, _ := claims["eml"].(string)
+	authConfig := AuthModel{
+		AccessToken: accessToken,
+		Email:       eml,
 	}
 
 	if err := fm.SaveToFile(authCacheFileName, authConfig); err != nil {
